d550000eb08176054ccf5aa09d928c5bd54617ab
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-06-09 10:13:15 -0700

    update files

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -2,9 +2,19 @@
 #include <stdlib.h> 
 #include <math.h> //must include to call math functions
 
+
 int main() {
 
+    // int luckyNumbers[] = {4, 8, 15, 64, 99, 9, 110};
+    // luckyNumbers[1] = 200; //assign int to index
 
+    // int luckyNumbers[10];
+    // luckyNumbers[1] = 80;
+    // luckyNumbers[0] = 90;
+
+    // printf("%d\n", luckyNumbers[0]);
+
+    char phrase[20];
 
     return 0;
 }
---
+++ functions/functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h> //must include to call math functions
+
+
+int main() {
+
+    
+
+    return 0;
+}
