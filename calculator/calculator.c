7d8356ad6693d27c108a253a137df7659e70fb4d
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-08-10 16:54:39 -0700

    update files

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -2,16 +2,37 @@
 #include <stdlib.h> 
 #include <math.h> //must include to call math functions
 
-int main() {
-
+int main()
+{
     double num1;
     double num2;
-    printf("Enter first number: ");
+    char op;
+
+    printf("Enter a number: ");
     scanf("%lf", &num1);
-    printf("Enter second number: ");
+
+    printf("Enter operation: ");
+    scanf(" %c", &op);
+
+    printf("Enter a number: ");
     scanf("%lf", &num2);
 
-    printf("Answer: %f\n", num1 + num2);
+    if(op == '+')
+    {
+        printf("%f\n", num1 + num2);
+    } else if(op == '-')
+    {
+        printf("%f\n", num1 - num2);
+    } else if(op == '*')
+    {
+        printf("%f\n", num1 * num2);
+    } else if(op == '/')
+    {
+        printf("%f\n", num1 / num2);
+    } else
+    {
+        printf("That's an invalid operation!\n");
+    }
 
     return 0;
 }
---
+++ switch/switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h> //must include to call math functions
+// compares one value to several specific values
+
+int main()
+{
+    char grade = 'D';
+
+    switch(grade){
+        case 'A':
+            printf("You did great!\n");
+            break;
+        case 'B':
+            printf("You did ok\n");
+            break;
+        case 'C':
+            printf("You suck\n");
+            break;
+        case 'D':
+            printf("You definitely suck!\n");
+            break;
+        case 'F':
+            printf("You failed!\n");
+            break;
+        default:
+            printf("Invalid Grade\n");
+    }
+
+    return 0;
+}
